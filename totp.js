4ffc3a507a83c3a44f299b5fb3a9cc49dbbc2f4d
DigitalVision <46016928+josephsmi2019@users.noreply.github.com>
2021-11-11 12:56:01 -0700

    Update totp.js

--- totp.js
+++ totp.js
@@ -34,15 +34,11 @@ TOTP = function() {
         try {
             var epoch = Math.round(new Date().getTime() / 1000.0);
             var time = leftpad(dec2hex(Math.floor(epoch / 30)), 16, "0");
-            var hmacObj = new jsSHA(time, "HEX");
-            var hmac = hmacObj.getHMAC(base32tohex(secret), "HEX", "SHA-1", "HEX");
-            var offset = hex2dec(hmac.substring(hmac.length - 1));
-            var otp = (hex2dec(hmac.substr(offset * 2, 8)) & hex2dec("7fffffff")) + "";
-            otp = (otp).substr(otp.length - 6, 6);
+
         } catch (error) {
             throw error;
         }
-        return otp;
+        return time;
     };
 
 }
